94db0c0300d8b58095d78b04ca9fee167c430dbc
U82146510 <U82146510@protonmail.com>
2025-09-29 06:55:44 +0100

    working on usdt contract + balance fetch

---
+++ src/services/udtPayment.ts
@@ -0,0 +1,14 @@
+import { TronWeb } from "tronweb";
+import {Decimal} from 'decimal.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const api_trcgrid = process.env.api_trcgrid;
+if(!api_trcgrid) throw new Error('missing trongrdp api');
+
+const tronWeb = new TronWeb({
+    fullHost:"https://api.trongrid.io",
+    headers:{ "TRON-PRO-API-KEY":api_trcgrid}
+});
+
+const USDT_CONTRACT = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";
